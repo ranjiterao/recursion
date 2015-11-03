a2e932d5f8f19da436c349ba0d9370c5702866e5
Ranjit Rao <ranjiterao@gmail.com>
2015-11-02 20:57:24 -0800

    Passes stringifyJSON test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,53 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  // numbers and booleans
+  if (typeof obj === "number" || typeof obj === "boolean") {
+  	var obj = obj.toString();
+  	return obj;
+  }
+
+  // null  
+  else if (obj === null) {
+  	return "null";
+  } 
+
+  // strings
+  else if (typeof obj === "string") {
+  	return '"' + obj + '"';
+  }
+
+  // arrays
+  else if (Array.isArray(obj)) {
+  	var newArr = [];
+
+  	for (var i = 0; i < obj.length; i++) {
+  		// recursive case 
+  		newArr.push(stringifyJSON(obj[i]));
+  	}
+
+  		// console.log(newArr); 
+  		// base case: add double quotes and brackets to front and back, eliminate spaces between elements
+  		return "[" + newArr.join(",") + "]";
+  }
+
+  // objects 
+  else if (typeof obj === "object") {
+  	var newArr = [];
+
+  	for (var key in obj) {
+
+  		// rule out objects that are undefined and that are functions 
+  		if (obj[key] !== undefined && typeof obj[key] !== "function") {
+
+  			// recursive case 
+	  		newArr.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+	  	}
+	  }
+
+	  // base case: add double quotes and curly braces to front and back, eliminate spaces between properties
+  	return "{" + newArr.join(",") + "}";
+  }
+
 };
